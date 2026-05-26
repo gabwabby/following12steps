6d03b3486cc7740a352e7e9a11b58560b1596f9e
gabwabby <gabmerino31@gmail.com>
2026-05-26 18:31:53 -0400

    Update step5.py

--- step5.py
+++ step5.py
@@ -21,20 +21,19 @@ un = numpy.ones((ny, nx))
 ##set hat function I.C. : u(.5<=x<=1 && .5<=y<=1 ) is 2
 u[int(.5 / dy):int(1 / dy + 1),int(.5 / dx):int(1 / dx + 1)] = 2 
 
-#fig = plt.figure(figsize=(11, 7), dpi=100)
-#ax = fig.add_subplot(projection='3d')                      
-#X, Y = numpy.meshgrid(x, y)                            
-#surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
-#plt.show()
+fig = plt.figure(figsize=(11, 7), dpi=100)
+ax = fig.add_subplot(projection='3d')                      
+X, Y = numpy.meshgrid(x, y)                            
+surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
+plt.show()
 
-#check above against step5 image
 
 for n in range(nt + 1): 
     un = u.copy() 
     row, col = u.shape
     for j in range(1, row):
         for i in range(1, col):
-            u[j,i] = (un[j,i] + ( c * dt / dx * (un[j, i] - un[j, i-1])) - ( c * dt / dy * (un[j, i] - un[j - 1, i])))
+            u[j,i] = (un[j,i] - ( c * dt / dx * (un[j, i] - un[j, i-1])) - ( c * dt / dy * (un[j, i] - un[j - 1, i])))
             u[0,:] = 1
             u[-1,:] = 1
             u[:,0] = 1
@@ -45,5 +44,18 @@ ax = fig.add_subplot(projection='3d')
 X, Y = numpy.meshgrid(x, y)   
 surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
 plt.show()
+ 
+for n in range(nt + 1): 
+    un = u.copy() 
+    u[1:, 1:] = (un[1:, 1:] - (c * dt / dx * (un[1:, 1:] - un[1:, :-1])) -
+                              (c * dt / dy * (un[1:, 1:] - un[:-1, 1:])))
+    u[0,:] = 1
+    u[-1,:] = 1
+    u[:,0] = 1
+    u[:,-1] = 1
 
-# this is wrong bro
+fig = plt.figure(figsize=(11, 7), dpi=100)
+ax = fig.add_subplot(projection='3d')
+X, Y = numpy.meshgrid(x, y)
+surf2 = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
+plt.show()
